f90b806bba018a72e603af4d03ddc8fbccdaeb67
gabrielbazante <gabrielbazante7@gmail.com>
2023-02-08 15:44:22 -0300

    configuracoes no dabase e package.json

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose')
+mongoose.set("strictQuery", true);
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo', { useMongoClient: true })
+module.exports = mongoose.connect('mongodb://localhost/todo')
