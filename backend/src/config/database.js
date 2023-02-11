57d0a7ae495d0c1e571edbeeed2668b08d90cedc
gabrielbazante <gabrielbazante7@gmail.com>
2023-02-11 12:27:50 -0300

    Correção para funcionar de forma correta o mongodb

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", true);
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect('mongodb://127.0.0.1:27017/todo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
